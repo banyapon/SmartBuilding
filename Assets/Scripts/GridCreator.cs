3a6fc4669e31b81d0ae6427902af9875c168f540
Banyapon Poolsawas <daydevthailand@gmail.com>
2022-10-05 10:10:07 +0700

    Commit Main Grid System

---
+++ Assets/Scripts/GridCreator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCreator : MonoBehaviour
+{
+    public GameObject block1; 
+ 
+    public int worldWidth  = 10;
+    public int worldHeight  = 10;
+    public int i = 0;
+    public float spawnSpeed = 0;
+ 
+    void  Start () {
+        StartCoroutine(CreateWorld());
+    }
+ 
+    IEnumerator CreateWorld () {
+        for(int x = -(worldWidth-1); x < worldWidth; x++) {
+            yield return new WaitForSeconds(spawnSpeed);
+            for(int z = 0; z < worldHeight; z++) {  
+                i = z+1;              
+                yield return new WaitForSeconds(spawnSpeed);
+                GameObject block = Instantiate(block1, Vector3.zero, block1.transform.rotation) as GameObject;
+                block.transform.parent = transform;
+                block.transform.localPosition = new Vector3(x, 0, z-((worldWidth/2)-1));
+                block.gameObject.name = i.ToString();
+            }
+        }
+    }
+}
